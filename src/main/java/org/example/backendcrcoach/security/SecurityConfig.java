b286ce54a727dbac1da1573e57bb1fb4e5c568f1
ricitos2001 <cesar2001ricitos@gmail.com>
2026-03-17 20:24:43 +0100

    actualizacion del SecurityConfig para permitir el acceso a pruebas

--- src/main/java/org/example/backendcrcoach/security/SecurityConfig.java
+++ src/main/java/org/example/backendcrcoach/security/SecurityConfig.java
@@ -76,7 +76,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200", "https://frontend-crcoach.onrender.com"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:4200", "https://frontend-crcoach.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
