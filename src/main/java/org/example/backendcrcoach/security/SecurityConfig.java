c9b4d807d0087418da88f45f97cf7b96374e880c
ricitos2001 <cesar2001ricitos@gmail.com>
2026-03-18 16:48:32 +0100

    actualizacion del proyecto

--- src/main/java/org/example/backendcrcoach/security/SecurityConfig.java
+++ src/main/java/org/example/backendcrcoach/security/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/auth/authenticate", "/api/v1/auth/register", "/api/v1/auth/logout", "/api/v1/auth/password/**").permitAll()
                         .requestMatchers("/api/v1/users/email-exists/**", "/api/v1/users/username-exists/**").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/swagger-ui/index.html").permitAll()
-                        .requestMatchers("/api/v1/**").hasAnyRole("USER")
+                        .requestMatchers("/api/v1/battles", "/api/v1/cards", "/api/v1/goals", "/api/v1/player-cards", "/api/v1/player-decks", "/api/v1/player-profiles", "/api/v1/sessions", "/api/v1/snapshots", "/api/v1/users").hasAnyRole("USER")
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
