7aed9381900d1b136219d21e469263970482c58e
ricitos2001 <cesar2001ricitos@gmail.com>
2026-04-27 12:25:32 +0200

    feat: Update Clan entity to store external badgeId from Clash Royale API

--- src/main/java/org/example/backendcrcoach/domain/dto/ClanRequestDTO.java
+++ src/main/java/org/example/backendcrcoach/domain/dto/ClanRequestDTO.java
@@ -12,5 +12,8 @@ import lombok.Setter;
 public class ClanRequestDTO {
     private String tag;
     private String name;
+    // badgeId is optional on create requests but when available it should be the
+    // id provided by the Clash Royale API so we persist the real value.
+    private Long badgeId;
 }
 
